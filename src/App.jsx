19be2282703b60e2f49ac21045bbb825ccd99a49
Yamkela Ngwazi <yamkelangwazi@gmail.com>
2026-08-15 15:35:48 +0200

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from '../pages/login.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
